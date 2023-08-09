aafd47431f6e9198be7be8acc41d53fe32f2e3f9
SBarethiya <sbarethiya@umass.edu>
2023-08-09 17:31:13 -0400

    Updated

--- utils/plot.py
+++ utils/plot.py
@@ -11,7 +11,7 @@ def latent_space_plot(encoded, save_path):
     plt.cla()
     plt.clf()
     plt.scatter(a[:,0],a[:,1],c='r')
-    plt.savefig(f"{save_path}encoder_mean.png")
+    plt.savefig(f"{save_path}/encoder_mean.png")
     plt.show()
 
     # Latent_Mean
@@ -19,7 +19,7 @@ def latent_space_plot(encoded, save_path):
     plt.cla()
     plt.clf()
     plt.scatter(b[:,0],b[:,1],c='b',s=5)
-    plt.savefig(f"{save_path}encoder_variance.png")
+    plt.savefig(f"{save_path}/encoder_variance.png")
     plt.show()
 
     # Latent_variance
@@ -27,9 +27,119 @@ def latent_space_plot(encoded, save_path):
     plt.cla()
     plt.clf()
     plt.scatter(c[:,0],c[:,1],c='g',s=5)
-    plt.savefig(f"{save_path}plt_encoded.png")
+    plt.savefig(f"{save_path}/plt_encoded.png")
     plt.show()
 
     return None
 
+# losses plot
 
+# summarize history for loss
+def train_test_loss_plot(history, out_path):
+    """
+    History of the model it will have loss, val_loss and other metric
+    Plotting between the loss/val loss and epoch
+    """
+    
+    plt.cla()
+    plt.clf()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title(f'loss{out_path}')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'{out_path}/loss.png')
+#    plt.show()
+    return None
+# All loss plot at once
+
+
+# RMSD, Pearsons, and Spearmann
+def Testing_analysis_plot(summary, out_path):
+    
+    Spearmann_arr = summary['Spearmann']
+    Pearson_arr = summary['Pearson']
+    RMSD_arr = summary['RMSD']
+    hyper_together_arr = summary['hyper_together']
+
+    # Plot Spearmann
+    plt.cla()
+    plt.clf()
+    plt.plot(hyper_together_arr,Spearmann_arr )
+    plt.scatter(hyper_together_arr, Spearmann_arr )
+    plt.ylabel('hyperparameters')
+    plt.xlabel('Spearmann')
+    plt.savefig(f"{out_path}/Spearmann_arr.png")
+
+
+    # Plot Pearson
+    plt.cla()
+    plt.clf()
+    plt.plot(hyper_together_arr, Pearson_arr)
+    plt.scatter(hyper_together_arr, Pearson_arr)
+    plt.ylabel('hyperparameters')
+    plt.xlabel('Pearson')
+    plt.savefig(f"{out_path}/Pearson_arr.png")    
+    
+    # Plot RMSD
+    plt.cla()
+    plt.clf()
+    plt.plot(hyper_together_arr, RMSD_arr)
+    plt.scatter(hyper_together_arr, RMSD_arr)
+    plt.ylabel('hyperparameters')
+    plt.xlabel('RMSD')
+    plt.savefig(f"{out_path}/RMSD_arr.png")
+
+    return None
+        
+    
+
+# Post-training Analysis
+def Post_training_analysis_plot(rmsd, c_resids, rmsf, Rgyr, residues, helicity_ave, sheet_ave, out_path):
+
+    """
+    Plot all the analysis on the decoded trajectory
+    """    
+
+    # RMSD
+    plt.cla()
+    plt.clf()
+    plt.plot(rmsd[1],rmsd[2])
+    plt.ylabel("RMSD (C$\u03B1$) in $\AA$")
+    plt.xlabel('time (ns)')
+    plt.savefig(f"{out_path}/protein_rmsd.png")
+    
+    # RMSF
+    plt.cla()
+    plt.clf()
+    plt.plot(c_resids, rmsf)
+    plt.ylabel("RMSF (C$\u03B1$) in $\AA$")
+    plt.xlabel('residue')
+    plt.savefig(f"{out_path}/residue.png")
+
+    # Rg
+    plt.cla()
+    plt.clf()
+    plt.plot(Rgyr[:,0], Rgyr[:,1])
+    plt.ylabel(r"radius of gyration $R_G$ ($\AA$)")
+    plt.xlabel("time (ps)")
+    plt.savefig(f"{out_path}/Rg.png")
+
+    # Helicity
+    plt.cla()
+    plt.clf()
+    plt.plot(residues, helicity_ave)
+    plt.ylabel("Helicilty")
+    plt.xlabel('residues')
+    plt.savefig(f"{out_path}/helicity.png")
+
+    # Sheet 
+    plt.cla()
+    plt.clf()
+    plt.plot(residues, sheet_ave)
+    plt.ylabel("Sheet")
+    plt.xlabel('residues')
+    plt.savefig(f"{out_path}/Sheet.png")
+
+    return None
--- utils/train_and_eval.py
+++ utils/train_and_eval.py
@@ -8,7 +8,10 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import MDAnalysis as mda
 from tensorflow.keras.callbacks import Callback
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow import random
 import sys
+import datetime
 
 from utils.model import *
 from utils.plot import *
@@ -24,11 +27,16 @@ def training(**kwargs):
             "BATCH_SIZE": 64,
             "LATENT_DIM": 2,
             "NUM_HIDDEN_LAYER": 4,
-            "EPOCHS": 200
-            "RATE": 0.00001
+            "EPOCHS": 200,
+            "RATE": 0.00001,
+            "early_stopping": True,
+            "seed": 20
         }
 
     """
+
+    seed = kwargs['seed']
+    random.set_seed(seed)
     scaler = kwargs['scaler']
     x_test = kwargs['x_test']
     x_train = kwargs['x_train']
@@ -39,19 +47,35 @@ def training(**kwargs):
     NUM_HIDDEN_LAYER = kwargs['NUM_HIDDEN_LAYER']
     EPOCHS = kwargs['EPOCHS']
     RATE = kwargs['RATE']
+    early_stopping = kwargs['early_stopping']
+    outtraj_dirname = kwargs['outtraj_dirname']
 
     # Build VAE MODEL
     encoder, decoder, vae = build_vae(original_dim, LATENT_DIM, NUM_HIDDEN_LAYER, RATE)
     encoder.summary()
 
     # Train VAE MODEL
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
-    vae.fit(x=x_train, y=x_train,
-            shuffle=True,
-            epochs=EPOCHS,
-            validation_data=(x_test, x_test),
-            callbacks=[early_stopping])
+    log_dir = str(outtraj_dirname) + "/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+ 
+    if "early_stopping" == False:
 
+        history = vae.fit(x=x_train, y=x_train,
+                shuffle=True,
+                epochs=EPOCHS,
+                validation_data=(x_test, x_test),
+                callbacks = [tensorboard_callback])
+    else:
+        # If you want early stopping:
+        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
+        history = vae.fit(x=x_train, y=x_train,
+                shuffle=True,
+                epochs=EPOCHS,
+                validation_data=(x_test, x_test),
+                callbacks=[early_stopping, tensorboard_callback])
+
+    # Plot losses
+    plot1 = train_test_loss_plot(history, outtraj_dirname)
     # TEST 
     encoded = encoder.predict(x_test, batch_size=BATCH_SIZE)
     decoded = decoder.predict(encoded[0])
@@ -82,7 +106,8 @@ def training(**kwargs):
     return_dict['RMSD'] = RMSD
  
     return_dict['demap'] = demap_rad
-
+    return_dict['outtraj_dirname'] = outtraj_dirname
+    return_dic['hyper_together'] = f"B{BATCH_SIZE}LD{LATENT_DIM}HL{NUM_HIDDEN_LAYER}E{EPOCHS}R{RATE}"
     return return_dict
 
 # Sampling from Latent space
@@ -92,10 +117,30 @@ def training(**kwargs):
 
 def evaluate(encoded, demap, x_test, scaler):
     Spearmann, Pearson = cal_spearman(x_test, encoded[0])
-    RMSD = cal_rmsd(x_test, demap, encoded[0], scaler)
+    RMSD = cal_rmsd(x_test, demap)
     return Spearmann, Pearson, RMSD
 
-def demap_to_PDB(Ic_bonds, Ic_angles, Ec, torsion,pdb_temp, R):
+# Spearmann correlation and Pearson correlation
+def cal_spearman(data_original, data_encoded):
+    dist_encoded = np.square(euclidean_distances(data_encoded, data_encoded)).flatten()
+    dist_original = np.square(euclidean_distances(data_original, data_original)).flatten()
+    spearman = spearmanr(dist_original, dist_encoded)
+    pearson = pearsonr(dist_original, dist_encoded)
+    return spearman, pearson
+
+# RMSD in A
+def cal_rmsd(data_original, data_decoded):
+    """
+    Calculate RMSD between input and output array
+    Input eg. (test data, output data after inverse transformation, 
+    return mean RMSD and standard deviation 
+    """
+    data_decoded = data_decoded
+    a = np.sum(np.square(data_decoded*10 - data_original*10), axis=1)
+    rmsd = np.sqrt(a / (data_original.shape[1] // 3))
+    return np.mean(rmsd), np.std(rmsd)
+
+def demap_to_PDB(Ic_bonds, Ic_angles, Ec, torsion,pdb_temp, outtraj_dirname,R):
     Ic_bonds_angles_ave = np.average(np.concatenate([Ic_bonds, Ic_angles], axis=1), axis=0)
     i = 0
     file = []
@@ -109,18 +154,21 @@ def demap_to_PDB(Ic_bonds, Ic_angles, Ec, torsion,pdb_temp, R):
         # Write into PDB
         template = extract_template(pdb_temp)
         coorinates = load_coor(new_xyz,template)
-        write_file(f"{i}.pdb",coorinates)
+        write_file(f"{outtraj_dirname}/pdb/{i}.pdb",coorinates)
         file.append(f"{i}.pdb")
         i += 1  
-    with open("pickle_file.pkl", 'wb') as w:
+    with open(f"{outtraj_dirname}/pdb/pickle_file.pkl", 'wb') as w:
         pickle.dump(file, w)
     return None
 
-def PDB_to_XTC(pickle_file, template_file):
-    pdb_file = pickle.load(open(pickle_file,'rb'))
+def PDB_to_XTC(pickle_file, template_file, output_xtc):
+    """
+    
+    """
+    pdb_file = pickle.load(open(f"{output_xtc}/pdb/pickle_file.pkl",'rb'))
     reformat_pdbfiles = [(i) for i in pdb_file]
-    u = mda.Universe(template_file)
-    with mda.Writer(output_xtc, len(u.atoms)) as xtc_writer:
+    u = mda.Universe(f"{template_file}")
+    with mda.Writer(f"{output_xtc}/pdb/output_xtc.xtc", len(u.atoms)) as xtc_writer:
         for pdb_file1 in reformat_pdbfiles[0:]:
                 u.load_new(pdb_file1)  # Load each PDB file into the Universe
                 xtc_writer.write(u)
@@ -128,24 +176,14 @@ def PDB_to_XTC(pickle_file, template_file):
             os.system("rm %s" % temp_file)
     return None
 
-# Spearmann correlation and Pearson correlation
-def cal_spearman(data_original, data_encoded):
-    dist_encoded = np.square(euclidean_distances(data_encoded, data_encoded)).flatten()
-    dist_original = np.square(euclidean_distances(data_original, data_original)).flatten()
-    spearman = spearmanr(dist_original, dist_encoded)
-    pearson = pearsonr(dist_original, dist_encoded)
-    return spearman, pearson
 
-# RMSD in A
-def cal_rmsd(data_original, data_decoded, data_encoded, scaler):
-    data_decoded = data_decoded
-    #data_decoded = data_decoded.reshape(data_encoded.shape[0], -1)    
-    a = np.sum(np.square(data_decoded*10 - data_original*10), axis=1)
-    rmsd = np.sqrt(a / (data_original.shape[1] // 3))
-    return np.mean(rmsd), np.std(rmsd)
-
-# Decoded to Protein
+# Decoded xyz coordinates to Protein
 def extract_template(pdb_file):
+    """
+    Extract the original PDB template except coordinates
+    Input: Template PDB
+    Output: template without coordinates
+    """
     template = []
     file = open(pdb_file).readlines()
     for line in file:
@@ -156,6 +194,11 @@ def extract_template(pdb_file):
     return template
 
 def load_coor(coordinates, template):
+    """
+    Create a template with new coordinates
+    Input: output xyz coordinates and template without coordinates 
+    Output: template with new coordinates
+    """
     cur_cor = None
     cur_cor = template[:]
 
@@ -173,6 +216,10 @@ def load_coor(coordinates, template):
     return cur_cor
 
 def write_file(pdb_file,cur_cor):
+    """
+    Write the coordinates into PDB
+    Inputs: output file name and decoded template
+    """
     file = open(pdb_file, "w")
     for line in cur_cor:
         file.write(line)
