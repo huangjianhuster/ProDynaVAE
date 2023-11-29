97d581964c94ef28bd6ca78ede0d793387ffcd8c
Jian <jianhuang@umass.edu>
2023-11-29 12:47:30 -0500

    modify analysis

--- utils/traj_analysis.py
+++ utils/traj_analysis.py
@@ -6,7 +6,109 @@ import MDAnalysis as mda
 from MDAnalysis.analysis.dihedrals import Dihedral
 import numpy as np
 import matplotlib.pyplot as plt
+import MDAnalysis.analysis.rms as rms
+import mdtraj as md
 import parmed as pmd
+
+# RMSD calculation
+def traj_rmsd(psf, xtc, align_select, rmsd_list):
+    """
+    psf: PSF or TPR
+    xtc: already_aligned XTC or DCD
+    return rmsd_matrix
+        rmsd_matrix has a shape of (4, number_of_frames)
+        rmsd_matrix[0]: frames
+        rmsd_matrix[1]: time
+        rmsd_matrix[2]: rmsd of align_select
+        rmsd_matrix[3-]: rmsd of rmsd_list
+    """
+    u = mda.Universe(psf, xtc)
+    ref = mda.Universe(psf, xtc)
+    ref.trajectory[0]
+    R = mda.analysis.rms.RMSD(u,ref,select=align_select,groupselections=rmsd_list)
+    R.run()
+    return R.results.rmsd.T
+
+# RMSF calculation: only for C-alpha
+def traj_rmsf(psf, xtc):
+    """
+    psf: PSF or TPR
+    xtc: already_aligned XTC or DCD
+    return resid, rmsf_matrix
+    """
+    u = mda.Universe(psf, xtc)
+    # ref = mda.Universe(psf, xtc)
+    # ref.trajectory[0]
+    c_alphas = u.select_atoms('protein and name CA')
+    R = rms.RMSF(c_alphas).run()
+    rmsf_matrix = R.results.rmsf
+    return c_alphas.resnums, rmsf_matrix
+
+# radius of gyration (IDP protein)
+def traj_rg(psf, xtc):
+    """
+    psf: PSF or TPR
+    xtc: XTC or DCD
+    return Rgyr
+    """
+    u = mda.Universe(psf, xtc)
+    Rgyr = []
+    for ts in u.trajectory:
+        Rgyr.append( u.atoms.radius_of_gyration())
+    Rgyr = np.array(Rgyr)
+    return Rgyr
+
+# helicity & sheet
+def traj_ss(psf, xtc):
+    if xtc.endswith("xtc"):
+        traj = md.load(xtc, top=psf)
+    if xtc.endswith("dcd"):
+        traj = md.load_dcd(xtc, top=psf)
+#    residues = list(traj.topology.residues)
+    dssp = md.compute_dssp(traj, simplified=True)
+    helicity = np.where(dssp=='H', 1, 0)
+    helicity_ave = np.sum(helicity, 0) / helicity.shape[0]
+    sheet = np.where(dssp=='E', 1, 0)
+    sheet_ave = np.sum(sheet, 0) / sheet.shape[0]
+    return  helicity_ave, sheet_ave
+
+# End-to-end distance
+def endtoend(psf,dcd):
+    u = mda.Universe(psf,dcd)  # always start with a Universe
+    nterm = u.select_atoms('protein and name N')[0]  # can access structure via segid (s4AKE) and atom name
+    cterm = u.select_atoms('protein and name C')[-1]  # ... takes the last atom named 'C'
+    bb = u.select_atoms('protein and backbone')  # a selection (a AtomGroup)
+    ete = []
+    frame = []
+    for ts in u.trajectory:  # iterate through all frames
+        r = cterm.position - nterm.position  # end-to-end vector from atom positions
+        d = numpy.linalg.norm(r)   # end-to-end distance
+        frame.append(ts.frame)
+        ete.append(d)
+    return frame, ete
+
+# PCA analysis
+def PCA(psf,dcd):
+    aligner = align.AlignTraj(u, u, select='backbone', in_memory=True).run()
+    pc = pca.PCA(u, select='backbone', align=True, mean=None, n_components=None).run()
+    backbone = u.select_atoms('backbone')
+    n_bb = len(backbone)
+    return pc
+
+# write pdbs into trajectory <-- for VAE decoder-generated trajectory
+def pdbs2xtc(pdblist, out_xtc):
+    """
+    pdblist: list of pdb paths to be combined together into a single trajectory file
+    out_xtc: output trajectory path
+    """
+    # to determin PDB atoms
+    u = mda.Universe(pdblist[0])
+    with mda.Writer(out_xtc, len(u.atoms)) as xtc_writer:
+        for pdb in pdblist:
+            u.load_new(pdb)
+            xtc_writer.write(u)
+
+    return None
 
 # get bonds
 def get_bonds(psf, xtc, top, atom1_name, atom2_name, every=1, use_atom_type=True):
