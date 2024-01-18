dced0f7fb63ec0f4aabee42978a0618dce6b75ad
SBarethiya <sbarethiya@umass.edu>
2024-01-18 12:31:03 -0500

    corrected

--- utils/train_and_eval.py
+++ utils/train_and_eval.py
@@ -11,8 +11,10 @@ from scipy.stats import spearmanr, pearsonr
 
 import multiprocessing
 from multiprocessing import Process
+import subprocess
+import concurrent.futures
 
-from utils.model import *
+from utils.model_2 import *
 from utils.plot import *
 
 import warnings
@@ -55,45 +57,60 @@ def training(**kwargs):
 
     # Build VAE MODEL
     encoder, decoder, vae = build_vae(original_dim, LATENT_DIM, NUM_HIDDEN_LAYER, RATE)
-    encoder.summary()
 
     # Train VAE MODEL
     log_dir = str(outtraj_dirname) + "/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
- 
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath= f"{log_dir}/checkpoint",
+        save_weights_only=False,
+        save_freq="epoch",
+        monitor="loss",
+        mode="min",
+        save_best_only=True,
+        verbose=0,
+    ) 
     if "early_stopping" == False:
         history = vae.fit(x=x_train, y=x_train,
                 shuffle=True,
                 epochs=EPOCHS,
                 validation_split=0.2, # validation_data=(x_val, x_val),
                 verbose=2,
-                callbacks = [tensorboard_callback])
+                batch_size = BATCH_SIZE,
+                callbacks = [tensorboard_callback, model_checkpoint_callback])
     else:
         # If you want early stopping:
         early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
-        history = vae.fit(x=x_train, y=x_train,
+        history = vae.fit(x=x_train,#, y=x_train,
                 shuffle=True,
                 epochs=EPOCHS,
                 validation_split=0.2, # validation_data=(x_val, x_val),
                 verbose=2,
-                callbacks=[early_stopping, tensorboard_callback])
-
-
+                batch_size = BATCH_SIZE,
+                callbacks=[early_stopping, tensorboard_callback, model_checkpoint_callback])
     formatter = mpl.ticker.EngFormatter()
     all_hype = f"B{BATCH_SIZE}LD{LATENT_DIM}HL{NUM_HIDDEN_LAYER}E{EPOCHS}R{RATE}S{kwargs['split']}"
+
+    vae.save(f"{outtraj_dirname}/{all_hype}/vae")
+    encoder.save(f"{outtraj_dirname}/{all_hype}/encoder")
+    decoder.save(f"{outtraj_dirname}/{all_hype}/decoder")
     # Plot losses
+#    plot_label_clusters(vae, x_train, x_train)
     plot1 = train_test_loss_plot(history, outtraj_dirname, all_hype)
     # TEST 
-    encoded = encoder.predict(x_test, batch_size=BATCH_SIZE)
-    decoded = decoder.predict(encoded[0])
+    encoded = encoder.predict(x_test)#, batch_size=BATCH_SIZE)
+    decoded = decoder.predict(encoded[2])
     demap =  scaler.inverse_transform(decoded)     
     x_train = scaler.inverse_transform(x_train)
     x_test = scaler.inverse_transform(x_test)
     # Plot here
-    #latent_space_plot(encoded, save_path)
 
     # Evaluation 
-    Spearmann, Pearson, pv_spearman, pv_pearson, RMSD_mean, RMSD_std = evaluate(encoded, demap, x_test, scaler) 
+    Spearmann, Pearson, pv_spearman, pv_pearson, RMSD_mean, RMSD_std = evaluate(encoded, demap, x_test) 
+    print(Spearmann, Pearson, pv_spearman, pv_pearson, RMSD_mean, RMSD_std)
+
+    latent_space_plot(encoded, LATENT_DIM, RMSD_mean, f"{outtraj_dirname}/{all_hype}")
+
     # Create return dict
     return_dict = {}
     
@@ -102,6 +119,7 @@ def training(**kwargs):
     return_dict['NUM_HIDDEN_LAYER'] = NUM_HIDDEN_LAYER
     return_dict['EPOCHS'] = EPOCHS
     return_dict['RATE'] = RATE
+    return_dict['x_test'] = x_test
 
     return_dict['Spearmann'] = Spearmann
     return_dict['Pearson'] = Pearson 
@@ -117,13 +135,13 @@ def training(**kwargs):
 # Sampling from Latent space
 #def sample_latent():
 
-def evaluate(encoded, demap, x_test, scaler):
-    Spearmann, Pearson, pv_spearman, pv_pearson = cal_spearman(x_test, encoded[0])
+def evaluate(encoded, demap, x_test):
+    Spearmann, Pearson, pv_spearman, pv_pearson = cal_corr(x_test, encoded[2])
     RMSD_mean, RMSD_std = cal_rmsd(x_test, demap)
     return Spearmann, Pearson, pv_spearman, pv_pearson, RMSD_mean, RMSD_std
 
 # Spearmann correlation and Pearson correlation
-def cal_spearman(data_original, data_encoded):
+def cal_corr(data_original, data_encoded):
     dist_encoded = np.square(euclidean_distances(data_encoded, data_encoded)).flatten()
     dist_original = np.square(euclidean_distances(data_original, data_original)).flatten()
     spearman, pv_spearman = spearmanr(dist_original, dist_encoded)
@@ -168,6 +186,34 @@ def dihedral_demap_to_PDB(Ic_bonds, Ic_angles, Ec, torsion,pdb_temp, outtraj_dir
     pick_file = f"{new_fold}/pickle_file.pkl"
     return pick_file, new_fold
 
+def dihedraltoFull(pdb, demap, selection,new_fold):
+    # load a structure as normal, get first chain
+    parser = PDBParser()
+    myProtein = parser.get_structure("protein",pdb)
+    myChain = myProtein[0]
+    # compute bond lengths, angles, dihedral angles
+    myChain.atom_to_internal_coordinates(verbose=True)
+    pdb_files = []
+    if selection == 'phi' or selection == 'omega' :
+        phi =  [None for _ in range(len(demap))]
+        new_demap = np.concatenate((phi,demap),axis=1)
+    elif selection == 'psi':
+        psi =  [None for _ in range(len(demap))]
+        new_demap = np.concatenate((demap,psi),axis=1)
+    for j, aa in enumerate(demap):
+        resid = myChain.get_residues()
+        for i, r in enumerate(resid):
+            r.internal_coord.set_angle("phi", aa[i])
+        myChain.internal_to_atom_coordinates()
+        # write new conformation with PDBIO
+        pdb_files.append(f"{new_fold}/{j}myChain.pdb")
+        write_PDB(myProtein, f"{new_fold}/{j}myChain.pdb")
+    pick_file = f"{new_fold}/pickle_file.pkl"
+    with open(pick_file, 'wb') as w:
+        pickle.dump(pdb_files, w)
+    return pick_file
+        
+
 def cartesian_demap_to_PDB(demap, outtraj_dirname, pdb_temp):
     i = 0
     file = []
@@ -182,50 +228,63 @@ def cartesian_demap_to_PDB(demap, outtraj_dirname, pdb_temp):
     pick_file = f"{outtraj_dirname}/pickle_file.pkl"
     return pick_file
 
+def run_command(command):
+    subprocess.run(command, shell=True)
+
 # CA to fully-atomic
 def CAtoFull(pick_file, outtraj_dirname):
     pick_file1 = []
-    pdb_file = pickle.load(open(f"{pick_file}",'rb'))
-    k = 0
-    for i in range(0,len(pdb_file),12):
-        #sm1 = []
-        for j in range(k,12,1): 
-            sm = f'./scripts/ModRefiner-l/mcrefinement {outtraj_dirname}  scripts/ModRefiner-l {pdb_file[j]} {pdb_file[j]} 5'.split()
-            #sm1.append(f"p{j}")
-            sm1 = multiprocessing.Process(target=sm)
-            sm1.start()
-            sm1.join() 
-        emd1 = []
-        for j in range(k,12,1):
-            emd = f'./scripts/ModRefiner-l/mcrefinement {outtraj_dirname}  scripts/ModRefiner-l {pdb_file[j]} {pdb_file[j]} 100 5'.split()
-            emd1.append(f"p{j}")
-            emd1 = multiprocessing.Process(emd)
-            emd1.start()
-            emd1.join()
-            pick_file1.append(f"em{pdb_file[j]}")
-        k = k + 12
-    with open(f"{outtraj_dirname}/pickle_file_new.pkl", 'wb') as w:
-        pickle.dump(pdb_file, outtraj_dirname)
-    pick_file1 = f"{outtraj_dirname}/pickle_file_new.pkl"
-    return pick_file1
+    pdb_file = pickle.load(open(pick_file,'rb'))
 
-def PDB_to_XTC(pickle_file, template_file, output_xtc,time_s):
+    # CA to full atomic software directory path
+    ModRef_path = '/home/shrishti/Documents/Projects/IDP_ensemble/ProDynaVAE/ModRefiner-l'
+    os.chdir(ModRef_path)
+
+    k = 0
+    # uses 12 cpus in parallel    
+    for i in range(0,len(pdb_file[:10]),12):
+        # To add mainchain atoms from CA pdbs
+        commands = [f'./mcrefinement {outtraj_dirname} {ModRef_path} {pdb_file[j]} {pdb_file[j]} 5' for j in range(k, k+12, 1)]
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(run_command, commands)
+        # To build sidechains atoms from mainchain pdb
+        #commands1 = [f'./emrefinement {outtraj_dirname}  {ModRef_path} mc{pdb_file[j]} mc{pdb_file[j]} 100 5' for j in range(k, k+12, 1)]
+        #with concurrent.futures.ProcessPoolExecutor() as executor:
+        #    executor.map(run_command, commands1)
+        # Save names to pickle file
+        for j in range(k,k+12,1):
+            pick_file1.append(f"{outtraj_dirname}/mc{pdb_file[j]}")
+            os.system("rm %s" % f"{outtraj_dirname}/mc{pdb_file[j]}")
+        k += 12
+    for temp_file in pdb_file:
+        os.system("rm %s" % f"{outtraj_dirname}/{temp_file}")
+    pick_file1_name = f"{outtraj_dirname}/pickle_file_new.pkl"
+    with open(pick_file1_name, 'wb') as w:
+        pickle.dump(pick_file1, w)
+    return pick_file1_name
+
+def PDB_to_XTC(pickle_file, output_xtc,time_s):
     """
         Mutiple PDB to XTC conveter 
     """
-    pdb_file = pickle.load(open(f"{pickle_file}",'rb'))
-    reformat_pdbfiles = [(i) for i in pdb_file]
-    u = mda.Universe(template_file)
+    # Load PDB pickle file
+    pdb_file = pickle.load(open(pickle_file,'rb'))
+#    reformat_pdbfiles = [(i) for i in pdb_file]
 
+    # from pdbs to xtc
+    u = mda.Universe(f"{output_xtc}/{pdb_file[0]}")
     xtc_tmp = f"{output_xtc}/output_tmp.xtc"
-    xtc_file = f"{output_xtc}/output_xtc.xtc"  
+    xtc_file = f"{output_xtc}/decoder.xtc"  
     with mda.Writer(xtc_tmp, len(u.atoms)) as xtc_writer:
-        for pdb_file1 in reformat_pdbfiles[0:]:
-            u.load_new(f"{output_xtc}/{pdb_file1}")  # Load each PDB file into the Universe
+#        for pdb in reformat_pdbfiles[0:]:
+        for pdb in pdb_file[0:]:
+            u.load_new(f"{output_xtc}/{pdb}")  # Load each PDB file into the Universe
             xtc_writer.write(u)
 
     # Add time step in the trajectory
-    u2 = mda.Universe(template_file, xtc_tmp)
+    u2 = mda.Universe(pdb_file[0], xtc_tmp)
+    select_atoms = u2.select_atoms("all")
+    select_atoms.write(f"{output_xtc}/noH.pdb")
     for ts in u2.trajectory:
         ts.data['dt'] = time_s
         ts.data['time'] = ts.frame*time_s
@@ -233,12 +292,35 @@ def PDB_to_XTC(pickle_file, template_file, output_xtc,time_s):
     with mda.Writer(xtc_file, u2.atoms.n_atoms) as W:
         for ts in u2.trajectory:
             W.write(u2.atoms)
-    for temp_file in pdb_file:
-        os.system("rm %s" % f"{output_xtc}/{temp_file}")
+#    for temp_file in pdb_file:
+#        os.system("rm %s" % temp_file)
     os.system("rm %s" % xtc_tmp)
     return None
 
-def demap_to_xtc(psf, demap, remove_selection, out_xtc):
+def CA_XTC(pdb, new_fold, demap): 
+    pdbs_fold = f"{new_fold}/pdb"
+    if os.path.exists(pdbs_fold) == False:
+        path = os.path.join(pdbs_fold)
+        os.mkdir(pdbs_fold)
+    pickle_file = cartesian_demap_to_PDB(demap, pdbs_fold, pdb)
+    #pickle_file = CAtoFull(pickle_file, pdbs_fold)
+    PDB_to_XTC(pickle_file, pdbs_fold, 1)
+    return None
+
+def Dihedral_XTC(pdb, new_fold, demap, selection):
+    pdbs_fold = f"{new_fold}/pdb"
+    if os.path.exists(pdbs_fold) == False:
+        path = os.path.join(pdbs_fold)
+        os.mkdir(pdbs_fold)
+    pickle_file = dihedraltoFull(pdb, demap, selection, pdbs_fold)
+    PDB_to_XTC(pickle_file, pdbs_fold, 1)
+    return None
+
+def demap_to_xtc(psf, pdb, demap, remove_selection, out_xtc):
+    if remove_selection in ["phi", "psi", "omega"]:
+        Dihedral_XTC(pdb, new_fold, demap, selection)
+        return
+        
     u = mda.Universe(psf)
     nonH_atoms = u.select_atoms(remove_selection)   # example: "not name H*"
 
@@ -252,6 +334,10 @@ def demap_to_xtc(psf, demap, remove_selection, out_xtc):
     nonH.load_new(demap[0].reshape((len(nonH_atoms), 3)))
     nonH.select_atoms("all").write(f"{out_xtc}/noH.pdb")
 
+    #if remove_selection == "name CA":
+    #    CA_XTC(f"{out_xtc}/noH.pdb", out_xtc, demap)
+
+    #else:
     with mda.Writer(f"{out_xtc}/decoder.xtc", len(nonH.atoms)) as xtc_writer:
         for frame in range(num_frames):
             nonH.load_new(demap[frame].reshape((len(nonH_atoms), 3)))
